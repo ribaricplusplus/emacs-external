b3fdd291f2c8f3d86edd03b74513ca23963f0b71
Bruno Ribaric <br.ribaric@gmail.com>
2020-08-11 19:09:48 +0200

    Add PHP script for video notes

---
+++ src/videos/README.md
@@ -0,0 +1,3 @@
+# Videos
+
+PHP script that concatenates all videos.txt files, so that I can easily review all my notes from watching videos.
---
+++ src/videos/script.php
@@ -0,0 +1,44 @@
+<?php
+function sort_by_time($a, $b){
+    return $a[0] - $b[0];
+}
+$home = getenv("HOME");
+if (!$home){
+    echo "HOME environment variable not found.\n";
+    exit(1);
+}
+$config_contents = file_get_contents($home . "/.emacs-config.json");
+if ($config_contents === false) {
+    echo "Could not read ~/.emacs-config.json.\n";
+    exit(1);
+}
+$config = json_decode($config_contents, true);
+define("NOTEBOOK_PATH", $config["NOTEBOOK_PATH"]);
+$files = shell_exec("find " . NOTEBOOK_PATH . " -type f -iname videos.txt -or -iname video.txt");
+$files_array = explode("\n", $files);
+
+// Array of arrays, associating access times with path names
+$arr = [];
+foreach ($files_array as $file){
+    if ($file === "") {
+        continue;
+    }
+    $metadata = stat($file);
+    if ($metadata === false) {
+        echo "Failed to stat file.\n";
+        var_dump($metadata);
+        exit(1);
+    }
+    $modification_time = $metadata["mtime"];
+    array_push($arr, [$modification_time, $file]);
+}
+usort($arr, "sort_by_time"); // From oldest to newest
+$arr_length = count($arr);
+for($i = $arr_length - 1; $i >= 0; --$i){
+    $file = $arr[$i][1];
+    $contents = file_get_contents($file);
+    echo $file . "\n";
+    echo "==================================================\n";
+    echo $contents;
+    echo "\n\n\n";
+}
